c0262d234228e086cc4ad2cf6633883cf7bc330d
RuthNjeri <35260694+RuthNjeri6@users.noreply.github.com>
2021-06-01 11:22:59 +0300

    modified the import file

--- import.py
+++ import.py
@@ -1,12 +1,4 @@
-#!/usr/bin/python3
-import requests
-import json
 
-import functools
-
-url = 'http://localhost:8500/api/doc'
-
-root_id = "86164651-dd05-437a-a2ae-158ec3391ea7" 
 
 # sample
 child_objects = [
@@ -107,32 +99,4 @@ child_objects = [
 ]
 
 
-def mapper(child_object, parent_id):
-    # save the object
-    data = child_object
-    data["parent_id"] = parent_id
-    data["type"] = 'node'
 
-    res_data = requests.post(url, data = data, headers={
-        'Accept': 'application/json'
-    })
-
-    res_data = json.loads(res_data.text)
-    
-    # check for errors
-    print(res_data)
-
-    # obtain the id
-    newId = res_data['id']
-
-    # repeat for kids
-    if 'children' in child_object:
-        res_data['children'] = list(map(functools.partial(mapper, parent_id=newId), child_object['children']))
-
-    return res_data
-
-
-tree = list(map(functools.partial(mapper, parent_id=root_id), child_objects))
-
-
-# print(tree)
